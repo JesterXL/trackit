d4b74b183a8069e7e02cf4a52c6d1d5119000155
Jesse Warden <jesterxl@jessewarden.com>
2018-02-20 17:37:14 -0500

    working file upload

--- src/UploadImageForm.js
+++ src/UploadImageForm.js
@@ -27,26 +27,59 @@ export default class UploadIamgeForm extends Component {
         e.preventDefault();
         // TODO: do something with -> this.state.file
         log("this.state.file:", this.state.file);
-        this.getBase64(this.state.file)
-        .then(result => {
-            const base64result = result.split(',')[1];
-            const data = {'image': base64result};
-            // const url = 'http://localhost:8080/trackit/upload';
-            const url = 'https://rkk1uvw7la.execute-api.us-east-1.amazonaws.com/prod/trackit/upload';
-            fetch(url, {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: new Headers({
-                    'Content-Type': 'application/json'
-                })
-            }).then(res => res.json())
-        })
-        .then(result => {
-            // this.fileInput.nativeElement.value = '';
-            log("result:", result);
-        })
-        .catch(error => log("error:", error));
+        // this.getBase64(this.state.file)
+        // .then(result => {
+        //     const base64result = result.split(',')[1];
+        //     const data = {'image': base64result};
+        //     // const url = 'http://localhost:8080/trackit/upload';
+        //     const url = 'https://rkk1uvw7la.execute-api.us-east-1.amazonaws.com/prod/trackit/upload';
+        //     fetch(url, {
+        //         method: 'POST',
+        //         body: JSON.stringify(data),
+        //         headers: new Headers({
+        //             'Content-Type': 'application/json'
+        //         })
+        //     }).then(res => res.json())
+        // })
+        // .then(result => {
+        //     // this.fileInput.nativeElement.value = '';
+        //     log("result:", result);
+        // })
+        // .catch(error => log("error:", error));
+
+        // encodeURIComponent
+        const file = this.state.file;
+        this.getSignedURL(file);
       }
+
+    getSignedURL(file) {
+        const url = `http://localhost:5000/sign-s3?file-name=${file.name}&file-type=${file.type}`;
+        return fetch(url, {
+            method: 'GET'
+        })
+        .then(result => result.json())
+        .then(result => {
+            // log("result:", result);
+            log("result:", result);
+            return this.putImageOnS3(file, result.signedRequest, result.url);
+        })
+        .catch(error => {
+            log("error:", error);
+        });
+    }
+
+    putImageOnS3(file, signedRequest, url) {
+        return fetch(signedRequest, {
+            method: 'PUT',
+            body: file
+        })
+        .then(result => {
+            log("putImageOnS3::result:", result);
+        })
+        .catch(error => {
+            log("putImageOnS3::error:", error);
+        })
+    }
 
     _handleImageChange(e) {
         e.preventDefault();
